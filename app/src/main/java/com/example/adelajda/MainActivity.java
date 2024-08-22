93c2bbc9357504ed1eb424e11da3f168f5efd0df
Maciej Guzewski <maciejguzewski@gmail.com>
2024-08-22 21:04:42 +0200

    Basic logic of MainActivity as well as activity_main layout.

--- app/src/main/java/com/example/adelajda/MainActivity.java
+++ app/src/main/java/com/example/adelajda/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.adelajda;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
+    private Button addList;
+    private ListView listOfLists;
+    private ArrayAdapter<String> adapter;
+    private Vector<String> listNames;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,53 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        restoreDataFromFile();
+
+        addList=findViewById(R.id.add_new_list);
+
+        listOfLists=findViewById(R.id.list_of_lists);
+        adapter = new ArrayAdapter<>(this, R.layout.list_row, listNames);
+        listOfLists.setAdapter(adapter);
+
+        addList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //activity new list
+            }
+        });
+
+        listOfLists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem =  parent.getItemAtPosition(position).toString();
+                //activity show list
+            }
+        });
+
+    }
+
+
+    private void restoreDataFromFile()
+    {
+        String fileName="ListNamesData.txt";
+        listNames=new Vector<>();
+        listNames.clear();
+
+        File file = getFileStreamPath(fileName);
+        if(file.exists()==false)
+        {
+            try (FileOutputStream fileOutputStream = openFileOutput(fileName, this.MODE_PRIVATE)) {}
+            catch (IOException e) {e.printStackTrace();}
+        }
+        else
+        {
+            try(FileInputStream fileInputStream = this.openFileInput(fileName);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream))) {
+                String bufferLine;
+                while ((bufferLine = reader.readLine()) != null) listNames.add(bufferLine);
+            } catch (IOException e) { e.printStackTrace(); }
+        }
+
     }
 }
